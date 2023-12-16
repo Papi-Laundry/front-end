2e919b344a4ddee283f3fd5c141fafb5b416fcb2
preettypeanuts <mrafly212001@gmail.com>
2023-12-16 17:44:06 +0700

    maps view unsolved

--- PapiLaundry/screens/MapsScreen.js
+++ PapiLaundry/screens/MapsScreen.js
@@ -1,10 +1,21 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // Mengimpor PROVIDER_GOOGLE untuk menggunakan Google Maps
 import { styles } from '../styles/style';
 
 export default function MapsScreen({ navigation }) {
-    return (
-        <View style={styles.bgContainer}>
-            <Text>Maps Screen</Text>
-        </View>
-    )
+  return (
+    <View style={styles.bgContainer}>
+      <MapView
+        // style={styles.map} // Menambahkan gaya dari styles untuk menentukan tata letak peta
+        provider={PROVIDER_GOOGLE} // Menentukan penyedia peta
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
+  );
 }
