480fce82ed83bd7d963eac0bcb3b699937c1cd8e
preettypeanuts <mrafly212001@gmail.com>
2023-12-18 09:56:30 +0700

    fix error fetch

--- PapiLaundry/components/Cards.js
+++ PapiLaundry/components/Cards.js
@@ -9,11 +9,11 @@ export const Cards = ({navigation, route, onPress}) => {
             <Image style={styles.image} source={{ uri: 'https://images.unsplash.com/photo-1626806819282-2c1dc01a5e0c?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }} />
 
             <View style={styles.details}>
-                <Text style={styles.title}>{name}</Text>
-                <Text style={styles.subtitle}>{subtitle}</Text>
+                <Text style={styles.title}>Laundry</Text>
+                <Text style={styles.subtitle}>Laundry</Text>
                 <View style={styles.meters}>
                     <Ionicons name="location" />
-                    <Text>{distance} Away</Text>
+                    <Text>100Km Away</Text>
                 </View>
             </View>
         </TouchableOpacity>
