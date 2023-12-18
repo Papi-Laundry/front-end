0e2e630c0e8fcdefa98e5667b4ddb2dfa32564fd
nokatotedo <dwikypanduaqri22@gmail.com>
2023-12-19 04:12:39 +0700

    refactor code

--- PapiLaundry/screens/HomeScreen.js
+++ PapiLaundry/screens/HomeScreen.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { ScrollView, Text, View, Dimensions } from "react-native";
+import React, { useContext, useEffect, useState } from 'react';
+import { ScrollView, View, } from "react-native";
 import { styles } from '../styles/style';
 import { Categories } from '../components/Categories';
 import { Searchbar } from '../components/Searchbar';
@@ -7,47 +7,97 @@ import { CarouselImage } from '../components/Carousel';
 import { Cards } from '../components/Cards';
 import { CardTitle } from '../components/CardTitle';
 import axios from 'axios';
-import { Card } from 'react-native-paper';
+import { LoginContext } from '../context/LoginContext';
+import { UserContext } from '../context/UserContext';
+import * as Location from 'expo-location'
 
 export default function HomeScreen({ navigation }) {
+  const { getToken } = useContext(LoginContext)
+  const { user, setUser } = useContext(UserContext)
 
-    // const width = Dimensions.get('window').width;
-    const [cardData, setCardData] = useState([]);
+  // const width = Dimensions.get('window').width;
+  const [laundriesData, setLaundriesData] = useState([]);
 
-    // Fetch card data from the API
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://papi-laundry-server.nokatotedo.my.id/laundries');
-        // Assuming the API response contains an array of card data
-        setCardData(response.data);
-      } catch (error) {
-        console.error('Error fetching card data:', error.message);
+  const fetchLaundries = async () => {
+    try {
+      const response = await axios.get(`${process.env.EXPO_PUBLIC_SERVER_URL}/laundries?latitude=${user.location.latitude}&longitude=${user.location.longitude}`);
+      
+      setLaundriesData(response.data);
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  const fetchProfiles = async () => {
+    try {
+      const token = await getToken()
+      const response = await axios({
+        url: `${process.env.EXPO_PUBLIC_SERVER_URL}/profiles`,
+        method: 'GET',
+        headers: {
+          "Authorization": "Bearer " + token
+        }
+      })
+      
+      setUser(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getUserLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync()
+    if(status !== 'granted') {
+      setUser({
+        ...user,
+        error: "Permission Denied"
+      })
+      return
+    }
+
+    let location = await Location.getCurrentPositionAsync({
+      accuracy: Location.LocationAccuracy.Highest
+    })
+  
+    setUser({
+      ...user,
+      // latitude: location.coords.latitude,
+      // longitude: location.coords.longitude
+      location: {
+        latitude: -6.260457,
+        longitude: 106.781739
       }
-    };
-    useEffect(() => {
-      fetchData();
-    }, []); // The empty dependency array ensures this effect runs once when the component mounts
-    // console.log(cardData);
-    return (
-        <View style={styles.bgContainer}>
-            <ScrollView>
-                <Searchbar navigation={navigation}/>
-                <Categories />
-                <CarouselImage />
-                <CardTitle/>
-                {cardData.map((card) => (
-                <Cards
-                    onPress={() => navigation.navigate("LaundryScreen")}
-                    key={card.id}
-                    name={card.name}
-                    subtitle={card.subtitle}
-                    image={card.image}
-                    distance={card.distance}  // Contoh, jika data jarak tersedia di objek card
-                />
-                ))}
-                {/* <Cards onPress={() => navigation.navigate("LaundryScreen")}/> */}
-                {/* <Cards onPress={() => navigation.navigate("LaundryScreen")}/> */}
-            </ScrollView>
-        </View>
-    )
+    })
+  }
+
+  useEffect(() => {
+    fetchProfiles();
+  }, []);
+
+  useEffect(() => {
+    if(user) {
+      getUserLocation();
+    }
+    if(user?.location?.latitude && user.location?.longitude) {
+      fetchLaundries();
+    }
+  }, [user])
+
+  return (
+    <View style={styles.bgContainer}>
+      <ScrollView>
+        <Searchbar navigation={navigation}/>
+        <Categories />
+        <CarouselImage />
+        <CardTitle/>
+        {laundriesData.map((laundry) => (
+          <Cards
+            onPress={() => navigation.navigate("LaundryScreen", { laundry })}
+            key={laundry.id}
+            laundry={laundry}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  )
 }
